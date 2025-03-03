a026a1c036c8f00e550b92ce51185d4260bcc666
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-03-03 11:24:46 +0000

    floreur sauvage

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
-
-
+const navMenu =document.getElementById('nav-menu');
+const navLink =document.querySelectorAll('.nav-link');
+const hamburger=document.getElementById('hamburger');
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+hamburger.addEventListener('click',()=>{
+    navMenu.classList.toggle('left-[0]');
+    hamburger.classList.toggle('ri-close-large-line');
 
+    
+})
+navLink.forEach(link=>{
+    link.addEventListener('click',()=>{
+        navMenu.classList.toggle('left-[0]');
+        hamburger.classList.toggle('ri-close-large-line');
+    })  
+});
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
 
